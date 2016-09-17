18272b5ac0b0ee573d8e114a7d4f3535f6538af2
jesse-neto <jesse@neto.com.au>
2016-09-17 12:51:12 +1000

    Removed incorrect messages and added who are you command

--- index.js
+++ index.js
@@ -76,47 +76,6 @@ controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, mess
   bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
 })
 
-controller.hears(['sandwich', 'coffee' ],'direct_message,direct_mention,mention', function(bot, message) {
-
-  bot.api.reactions.add({
-    timestamp: message.ts,
-    channel: message.channel,
-    name: 'bec',
-  },function(err, res) {
-    if (err) {
-      bot.botkit.log('Failed to add emoji reaction :(',err);
-    }
-  });
-
-  bot.reply(message,'Do I look like I should be in a kitchen?');
-});
-
-controller.hears(['who are you', 'what are you'], ['direct_message', 'direct_mention,mention'], function(bot, message) {
-
-  bot.api.reactions.add({
-    timestamp: message.ts,
-    channel: message.channel,
-    name: 'davo',
-  },function(err, res) {
-    if (err) {
-      bot.botkit.log('Failed to add emoji reaction :(',err);
-    }
-  });
-
-  bot.reply(message,'I\'m the bastard love child of David O and Jesse C. You don\'t even want to start to imagine the things that occurred for me to brought into this terrible, awful world.');
-});
-
-controller.hears(['what is ecommerce', 'do you know anything about ecommerce', 'tell me about ecommerce'],'direct_message,direct_mention,mention',function(bot, message) {
-
-  bot.api.reactions.add({
-    timestamp: message.ts,
-    channel: message.channel,
-    name: 'package',
-  },function(err, res) {
-    if (err) {
-      bot.botkit.log('Failed to add emoji reaction :(',err);
-    }
-  });
-
-  bot.reply(message,'By definition e-commerce refers to businesses and consumers buying and selling products online. The majority of e-commerce websites on the internet are retail stores selling products directly to the public. However there are also a proportion of online stores dedicated to business-to-business (B2B) sales or wholesale activity. E-commerce does not only refer to the selling of physical products, it can also refer to the selling of services where payments for the services are made online. As a general rule an e-commerce website is a website where a transfer of funds is completed electronically - hence e-commerce.');
-});
+controller.hears(['who are you?' , 'what are you?'], ['direct_message'], function (bot, message) {
+  bot.reply(message, 'I\'m Neto Bot\. I\'m here to help you with anything Neto related!' )
+})
